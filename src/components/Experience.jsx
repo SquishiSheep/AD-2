0b963cfa762933ad38917983b3fa69ce515414d5
unknown <panditdipesh12@gmail.com>
2024-10-01 15:26:40 -0500

    updated the experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,22 +7,34 @@ export default function Experience(){
 
     const experience_list=[
         {
+            title:"Undergraduate Research Assistant",
+            company:"Texas State University",
+            date:"Sept 2024 - Present",
+            icon_url:"https://media.licdn.com/dms/image/v2/C560BAQGo7TTg-tgkdQ/company-logo_200_200/company-logo_200_200/0/1631300608004?e=1735776000&v=beta&t=GMkRxFVzGNm8hoFCGBXRaN5MiLGJtnGHUkCfdvtNY1Y"
+        },
+        {
             title:"Content Creator",
             company:"Tech DD Twins & DD Twins Vlog",
             date:"Oct 2019 - Present",
             icon_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvA3_1qY-FR18aAqmIYiLzkHOaB9I_JbF_eA&s"
         },
         {
-            title:"IT Manager",
+            title:"IT Assistant Intern",
             company:"SAB Trade Link & Suppliers Pvt. Ltd",
             date:"Jul 2023 - Dec 2023",
-            icon_url:"https://scontent.fktm7-1.fna.fbcdn.net/v/t39.30808-1/289809641_159352189984480_8542875292577041927_n.jpg?stp=dst-jpg_p200x200&_nc_cat=108&ccb=1-7&_nc_sid=f4b9fd&_nc_ohc=-lG7m-0Gu4wQ7kNvgEhl9nH&_nc_ht=scontent.fktm7-1.fna&oh=00_AYB1R5VFzEF5na9tl30Pnj5BRWUAaaGoJuuaSmINowW1zg&oe=6688006A"
+            icon_url:"https://scontent-dfw5-2.xx.fbcdn.net/v/t39.30808-6/289809641_159352189984480_8542875292577041927_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=cagn427dt8cQ7kNvgHs1Phd&_nc_ht=scontent-dfw5-2.xx&_nc_gid=AvDQXMqGmu9q_-Xzl0VZcXT&oh=00_AYA_0l1CYDQYL9tyXsYVsH6WtQ0pAK8mTb1AJTP2JqAk5w&oe=67022C30"
         },
         {
             title:"Founder/Web Developer",
             company:"Hamro Opportunity",
             date:"Jul 2022 - Dec 2023",
-            icon_url:"https://scontent.fktm7-1.fna.fbcdn.net/v/t39.30808-1/280377872_105122328872350_5483329207268016303_n.jpg?stp=dst-jpg_p200x200&_nc_cat=109&ccb=1-7&_nc_sid=f4b9fd&_nc_ohc=wdX53PFFuToQ7kNvgFmTBRT&_nc_ht=scontent.fktm7-1.fna&oh=00_AYC54bolAtesuEKKGQJDpOxfZlXDeEA0n7pr2N94dqSYBA&oe=6687F663"
+            icon_url:"https://scontent-dfw5-2.xx.fbcdn.net/v/t39.30808-6/280377872_105122328872350_5483329207268016303_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=4Gvs2kQv6LgQ7kNvgF7WMZu&_nc_ht=scontent-dfw5-2.xx&oh=00_AYAB2bDA9h6v6zRhitDNbatjo_TMn6xiYY1GMRgTVQHCjw&oe=670236A5"
+        },
+        {
+            title:"President - Computer Club",
+            company:"White House International Secondary School/College",
+            date:"May 2022 - May 2023",
+            icon_url:"https://media.licdn.com/dms/image/v2/C510BAQE-mW5UnUKTWg/company-logo_200_200/company-logo_200_200/0/1630608296527/whitehousegsm_logo?e=1735776000&v=beta&t=0oCqSIrdU4VUQeqSYGADDDsgOc2hUCyCQqcBw0QuBjQ"
         }
     ]
 
